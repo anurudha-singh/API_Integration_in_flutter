6401bd72834cfa2637a3617a271ba70a92cc5b91
Lucifer <singhanurudh570@gmail.com>
2023-02-04 09:28:07 +0530

    Exception statement comment added

--- lib/api_Service.dart
+++ lib/api_Service.dart
@@ -15,7 +15,7 @@ class Api_Services{
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
-      throw Exception('Failed to load album');
+      throw Exception('Failed to load album');//Exception statement comment
     }
   }
 
